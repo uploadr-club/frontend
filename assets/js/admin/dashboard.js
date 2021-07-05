4a8d29d259d14b69eb944bf7fcb2bba32ca95627
primus <git@epfforce.tech>
2021-07-05 20:22:48 +0100

    continue removing extra guts of the admin page

---
+++ assets/js/admin/dashboard.js
@@ -0,0 +1,25 @@
+async function getUserData() {
+    let headers = {
+        Authorization: localStorage.getItem("token"),
+    };
+    let req = await fetch("https://api.uploadr.club/api/v1/user/data", {
+        headers: headers,
+    });
+    return req.json();
+}
+
+function loadData() {
+    getUserData().then((data) => {
+        if (data.error) {
+            $("#total_files")[0].innerText = data.error.description;
+        } else {
+            // noinspection JSUnresolvedVariable
+            $(
+                "#userinfo"
+            )[0].innerText = `UUID: ${data.user.uuid}\nFile Limit Enabled: ${data.ufs.enabled}`;
+        }
+    });
+
+
+}
+loadData();
